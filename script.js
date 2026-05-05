060ae9af0e909d4fee3057be2f2504037365f824
Michael Wooden <wooden5730@msn.com>
2026-05-05 18:23:29 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Mobile menu toggle
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.style.display =
+    navLinks.style.display === "flex" ? "none" : "flex";
+});
